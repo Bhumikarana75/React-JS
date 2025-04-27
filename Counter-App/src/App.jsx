b9eef2b6af39f9f5c183b1477cc4e66bca9b97d9
Bhumikarana75 <bhumikarana958@gmail.com>
2025-04-27 23:21:16 +0530

    React JS

--- Counter-App/src/App.jsx
+++ Counter-App/src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
+import {Counter} from "./Counter";
+
 
 function App() {
+  const [no, setNo] = useState(0);
+  
+  const Incre = () => {
+    setNo(no + 1);
+  }
+
+  const Decre = () => {
+    setNo(no - 1);
+  }
+
+  const Reset = () => {
+    setNo(0);
+  }
 
   return (
-    <>
-      <counter/>
-    </>
+    <Counter
+      no = {no}
+      plus = {Incre}
+      minus = {Decre}
+      reset = {Reset}
+    />
   )
 }
 
-export default App
+export default App;
