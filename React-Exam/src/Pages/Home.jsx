0f435fafbeadbcf1b256fb5e0abc4b78b734e012
Bhumikarana75 <bhumikarana958@gmail.com>
2025-07-11 11:02:38 +0530

    React JS

---
+++ React-Exam/src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// import "./Home.css"
+
+const Navbar = () => (
+  <>
+    <nav class="nav">
+  <a class="nav-link active" aria-current="page" href="#">Active</a>
+  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+</nav>
+  </>
+);
+
+export default Navbar;
